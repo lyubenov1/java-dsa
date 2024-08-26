2c5af085045176532f1486f4403e67802606614f
lyubenov1 <blyubenov9@gmail.com>
2024-08-26 21:57:33 +0300

    Initial commit

---
+++ src/main/java/com/test/ArraysAndHashing/ContainsDuplicate.java
@@ -0,0 +1,25 @@
+package com.test.ArraysAndHashing;
+
+import java.util.*;
+
+public class ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 1};
+
+        boolean result = solution(nums);
+        System.out.println(result);
+    }
+
+    static boolean solution(int[] nums) {
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                return true;
+            }
+            seen.add(num);
+        }
+        return false;
+    }
+
+}
