58c1cf05a2e553da4951aaa7e38857f271074347
lyubenov1 <blyubenov9@gmail.com>
2024-08-30 16:47:29 +0300

    Another solution and comment

--- src/main/java/com/test/SlidingWindow/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/test/SlidingWindow/BestTimeToBuyAndSellStock.java
@@ -4,10 +4,22 @@ public class BestTimeToBuyAndSellStock {
 
     public static void main(String[] args) {
         int[] prices = {7,1,5,3,6,4};
-        int result = solution(prices);
-        System.out.println(result);
+
+        // Expected: 5 (profit: 6 - 1 == 5)
+        int maxProfit = solution(prices);
+        System.out.println(maxProfit);
     }
 
+    public static int solution(int[] prices) {
+        int max = 0, min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            if (price < min) min = price;
+            else if (price > min) max = Math.max(price - min, max);
+        }
+        return max;
+    }
+
+    /*
     private static int solution(int[] prices) {
         int l = 0, r = 1;
         int maxProfit = 0;
@@ -24,4 +36,6 @@ public class BestTimeToBuyAndSellStock {
 
         return maxProfit;
     }
+
+     */
 }
