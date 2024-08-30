79de6b65b0cf1c6ab095873cad6a06e0da697302
lyubenov1 <blyubenov9@gmail.com>
2024-08-30 13:11:38 +0300

    Valid palindrome solved with two pointers

---
+++ src/main/java/com/test/TwoPointers/ValidPalindrome.java
@@ -0,0 +1,37 @@
+package com.test.TwoPointers;
+
+public class ValidPalindrome {
+
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        boolean isPalindrome = solution(s);
+        System.out.println(isPalindrome);
+    }
+
+    private static boolean solution(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+
+        int start = 0;
+        int last = s.length() - 1;
+
+        while(start <= last) {
+            char currLeft = s.charAt(start);
+            char currRight = s.charAt(last);
+
+            if (!Character.isLetterOrDigit(currLeft)) {
+                start++;
+            } else if (!Character.isLetterOrDigit(currRight)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currLeft) != Character.toLowerCase(currRight)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+    }
+}
