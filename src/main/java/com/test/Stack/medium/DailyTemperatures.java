13693eba814e9d4594dc9815d2458d3a655141fa
lyubenov1 <blyubenov9@gmail.com>
2024-09-01 22:32:56 +0300

    Daily temperatures stack problem

---
+++ src/main/java/com/test/Stack/medium/DailyTemperatures.java
@@ -0,0 +1,43 @@
+package com.test.Stack.medium;
+
+import java.util.*;
+
+public class DailyTemperatures {
+
+    public static void main(String[] args) {
+        int[] temperatures = {73, 74, 75, 71, 69, 72, 76, 73};
+        int[] output = solution(temperatures);
+        System.out.println(Arrays.toString(output));
+    }
+
+    private static int[] solution(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                int idx = stack.pop();
+                ret[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        return ret;
+    }
+
+
+    // Solution using an array
+    /*
+    private static int[] solution(int[] temperatures) {
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
+            }
+            stack[++top] = i;
+        }
+        return ret;
+    }
+     */
+}
