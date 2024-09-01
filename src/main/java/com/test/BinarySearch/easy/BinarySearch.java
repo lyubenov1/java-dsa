1f946d177fc4dc964c5ebeb20ffb9fa15ba44b21
lyubenov1 <blyubenov9@gmail.com>
2024-09-02 00:57:28 +0300

    Classic binary search problem

---
+++ src/main/java/com/test/BinarySearch/easy/BinarySearch.java
@@ -0,0 +1,30 @@
+package com.test.BinarySearch.easy;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        int result = solution(nums, target);
+        System.out.println(result);
+    }
+
+    private static int solution(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2; // calculate the middle index of the array
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1; // target not found in the array
+    }
+}
