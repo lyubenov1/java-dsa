ff3531d0e859ec102beb6f935d220733b60d6179
lyubenov1 <blyubenov9@gmail.com>
2024-09-01 14:24:41 +0300

    Min stack problem with three different solutions

---
+++ src/main/java/com/test/Stack/medium/MinStack.java
@@ -0,0 +1,108 @@
+package com.test.Stack.medium;
+
+class MinStack {
+    private Node head;
+
+    public void push(int x) {
+        if (head == null)
+            head = new Node(x, x, null);
+        else
+            head = new Node(x, Math.min(x, head.min), head);
+    }
+
+    public void pop() {
+        head = head.next;
+    }
+
+    public int top() {
+        return head.val;
+    }
+
+    public int getMin() {
+        return head.min;
+    }
+
+    private static class Node {
+        int val;
+        int min;
+        Node next;
+
+        private Node(int val, int min, Node next) {
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        MinStack minStack = new MinStack();
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin()); // return -3
+        minStack.pop();
+        System.out.println(minStack.top()); // return 0
+        System.out.println(minStack.getMin()); // return -2
+    }
+}
+
+
+// Second solution - using a single stack
+/*
+class MinStack {
+    int min = Integer.MAX_VALUE;
+    Stack<Integer> stack = new Stack<Integer>();
+    public void push(int x) {
+        // only push the old minimum value when the current
+        // minimum value changes after pushing the new value x
+        if(x <= min){
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        // if pop operation could result in the changing of the current minimum value,
+        // pop twice and change the current minimum value to the last minimum value.
+        if(stack.pop() == min) min=stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+}
+ */
+
+
+// Third solution - using two stacks
+/*
+class MinStack {
+    Stack<Integer> min = new Stack<>();
+    Stack<Integer> stack = new Stack<>();
+    public void push(int x) {
+        stack.push(x);
+        if (min.isEmpty() || min.peek() >= x) {
+            min.push(x);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(min.peek())) {
+            min.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
+ */
