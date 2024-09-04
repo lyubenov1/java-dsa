1eeb8790a00c3d054a11c428ee688dec125d4e0f
lyubenov1 <blyubenov9@gmail.com>
2024-09-04 23:46:19 +0300

    Remove duplicates from sorted array

---
+++ src/main/java/com/test/TwoPointers/easy/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,21 @@
+package com.test.TwoPointers.easy;
+
+public class RemoveDuplicatesFromSortedArray {
+    public static void main(String[] args) {
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int output = solution(nums);
+        System.out.println(output);
+    }
+
+    private static int solution(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+}
+
