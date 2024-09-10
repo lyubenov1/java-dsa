15e1d780056847867aa86f0da3a632822ce2cdac
lyubenov1 <blyubenov9@gmail.com>
2024-09-10 19:41:15 +0300

    Add iterative solution

--- src/main/java/com/test/LinkedList/easy/MergeTwoSortedLists.java
+++ src/main/java/com/test/LinkedList/easy/MergeTwoSortedLists.java
@@ -5,6 +5,33 @@ import com.test.LinkedList.easy.ReverseLinkedList.*;
 public class MergeTwoSortedLists {
 
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0); // Create a dummy node as the starting point
+        ListNode handler = head; // Handler will help us link nodes together
+
+        while (l1 != null && l2 != null) { // Traverse through both lists while neither is null
+            if (l1.val <= l2.val) { // If the value in l1 is less than or equal to the value in l2
+                handler.next = l1; // Point handler to the smaller node (l1 in this case)
+                l1 = l1.next; // Move l1 to the next node
+            } else { // Otherwise, point handler to the node in l2
+                handler.next = l2;
+                l2 = l2.next; // Move l2 to the next node
+            }
+            handler = handler.next; // Move handler to the next node in the merged list
+        }
+
+        // If one list is exhausted, link the remaining part of the other list
+        if (l1 != null) {
+            handler.next = l1;
+        } else if (l2 != null) {
+            handler.next = l2;
+        }
+
+        // Return the next node after the dummy head (i.e., the real head of the merged list)
+        return head.next;
+    }
+
+    /* Recursive Solution
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if (l1 == null) return l2;
         if (l2 == null) return l1;
 
@@ -16,6 +43,7 @@ public class MergeTwoSortedLists {
             return l2;
         }
     }
+     */
 
     public static void printList(ListNode node) {
         while (node != null) {
