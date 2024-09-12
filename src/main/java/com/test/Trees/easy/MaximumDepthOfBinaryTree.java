af6d46dd2639d1fb5696f6589bb05b7a5703577c
lyubenov1 <blyubenov9@gmail.com>
2024-09-12 19:38:26 +0300

    Correction

--- src/main/java/com/test/Trees/easy/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/test/Trees/easy/MaximumDepthOfBinaryTree.java
@@ -17,13 +17,26 @@ public class MaximumDepthOfBinaryTree {
     /*
     --------- BFS solution
     public static int maxDepth(TreeNode root) {
-        if (root == null) {
+        if(root == null) {
             return 0;
         }
-
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        return Math.max(left, right) + 1;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int count = 0;
+        while(!queue.isEmpty()) {
+            int size = queue.size();
+            while(size-- > 0) {
+                TreeNode node = queue.poll();
+                if(node.left != null) {
+                    queue.offer(node.left);
+                }
+                if(node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            count++;
+        }
+        return count;
     }
      */
 
