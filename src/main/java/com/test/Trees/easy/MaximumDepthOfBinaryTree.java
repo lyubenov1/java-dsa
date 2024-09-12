515dd1d936319711918c5d2188d88ff6bb89358a
lyubenov1 <blyubenov9@gmail.com>
2024-09-12 19:36:51 +0300

    Added bfs solution

--- src/main/java/com/test/Trees/easy/MaximumDepthOfBinaryTree.java
+++ src/main/java/com/test/Trees/easy/MaximumDepthOfBinaryTree.java
@@ -14,6 +14,19 @@ public class MaximumDepthOfBinaryTree {
         return Math.max(left, right) + 1;
     }
 
+    /*
+    --------- BFS solution
+    public static int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+     */
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(3,
                 new TreeNode(9),
