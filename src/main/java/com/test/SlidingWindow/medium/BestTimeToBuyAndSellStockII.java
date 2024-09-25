7c7b555b1608a083dce0b8adde3b91fe44f7926e
lyubenov1 <blyubenov9@gmail.com>
2024-09-25 21:02:57 +0300

    Best Time to Buy and Sell Stock II

---
+++ src/main/java/com/test/SlidingWindow/medium/BestTimeToBuyAndSellStockII.java
@@ -0,0 +1,26 @@
+package com.test.SlidingWindow.medium;
+
+public class BestTimeToBuyAndSellStockII {
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+
+        int totalProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                totalProfit += prices[i] - prices[i - 1];
+            }
+        }
+        return totalProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+
+        int maxProfit = maxProfit(prices);
+        System.out.println(maxProfit);
+    }
+
+}
