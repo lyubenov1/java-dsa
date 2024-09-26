a9c8e64eb8fc3f42faf3a8d2c424e579dcd25ae1
lyubenov1 <blyubenov9@gmail.com>
2024-09-26 10:52:59 +0300

    Jump game

--- src/main/java/com/test/ArraysAndHashing/easy/JumpGame.java
+++ src/main/java/com/test/ArraysAndHashing/easy/JumpGame.java
@@ -4,12 +4,12 @@ public class JumpGame {
     static boolean canJump(int[] nums) {
         int last = nums.length - 1;
 
-        for(int i = last; i >= 0; i--){
+        for(int i = last - 1; i >= 0; i--){
             if(i + nums[i] >= last) {
                 last = i;
             }
         }
-        return last <= 0;
+        return last == 0;
     }
 
     public static void main(String[] args) {
