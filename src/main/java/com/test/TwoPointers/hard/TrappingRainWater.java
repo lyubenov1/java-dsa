3429029321b1d2e85538a3a2f1250667e39e2e21
lyubenov1 <blyubenov9@gmail.com>
2024-09-28 11:49:58 +0300

    Trapping rain water

---
+++ src/main/java/com/test/TwoPointers/hard/TrappingRainWater.java
@@ -0,0 +1,40 @@
+package com.test.TwoPointers.hard;
+
+public class TrappingRainWater {
+
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = height.length - 1;
+        int maxLeft = 0, maxRight = 0;
+        int totalWater = 0;
+
+        while (left < right) {
+            if (height[left] < height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    totalWater += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    totalWater += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+
+        return totalWater;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        int result = trap(height);
+        System.out.println("The amount of trapped rainwater is: " + result);
+    }
+}
